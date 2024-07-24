4d9703bb1645d45ea8bbff3360af8786f9cec6b3
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-07-24 16:51:10 -0500

    defined the onChange and the onSubmit functions

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,19 @@
+const Login = ({ login, isAuthenticated }) => {{
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const { email, password } = formData
+
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        login(email, password)
+    }
+
+    
+}}
