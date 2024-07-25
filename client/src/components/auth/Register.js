179e134eac4c6e510715734578278e72565ba847
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-07-24 22:15:36 -0500

    defined the register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -6,7 +6,82 @@ import { register} from '../../actions/auth'
 import propTypes from 'prop-types'
 
 const Register = ({ setAlert, register, isAuthenticated }) => {
+    const [formData, setFormData] = useState({
+        name: '',
+        emai: '',
+        password: '',
+        confirmPass: ''
+    });
 
+    const { name, email, password, confirmPass } = formData
+
+    const onChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value });
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        if (password !== confirmPass) {
+            setAlert('passwords do not match', 'danger');
+        } else {
+            register({ name, email, password })
+        }
+    }
+
+    if (isAuthenticated) {
+        return <Navigate to="/dashboard"/>
+    }
+
+    return (
+        <section className="container">
+            <h1 className='large text-primary'>Sign up</h1>
+            <p className='lead'>
+                <i className='fas fa-user' /> create your account
+            </p>
+            <form  className='form' onSubmit={onSubmit}>
+                 <div className='form-group'>
+                    <input
+                        type="text"
+                        placeholder='name'
+                        name='name'
+                        value={name}
+                        onChange={onChange}
+                    />
+                 </div>
+                 <div className='form-group'>
+                    <input
+                        type="password"
+                        placeholder='password'
+                        name='password'
+                        value={password}
+                        onChange={onChange}
+                    />
+                    <small className='form-text'>
+                        This site uses Gravatar so if you want a profile image, use
+                        a Gravatar email
+                    </small>
+                 </div>
+                 
+                 <div className='form-group'>
+                    <input
+                        type="password"
+                        placeholder='confirm password'
+                        name='confirmPass'
+                        value={confirmPass}
+                        onChange={onChange}
+                    />
+                    <small className='form-text'>
+                        This site uses Gravatar so if you want a profile image, use
+                        a Gravatar email
+                    </small>
+                 </div>
+            </form>
+            <p className='my-1'>
+                Already have an account? <Link to="/login">Sign in</Link>
+            </p>
+        </section>
+    )
 }
 
 Register.propTypes = {
