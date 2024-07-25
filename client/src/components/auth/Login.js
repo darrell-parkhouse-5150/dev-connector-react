b2d93d5b4c12e9cdc7eecd7e84b5fa7f2f9cce53
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-07-24 20:26:07 -0500

    defined the structure for the form and defined the proptypes

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -18,4 +18,48 @@ const Login = ({ login, isAuthenticated }) => {{
         login(email, password)
     }
 
-}}
+    if (isAuthenticated) {
+        return <Navigate to="/dashboard" />
+    }
+
+    return (
+        <section className="container">
+            <h1 class="large text-primary">Sign in</h1>
+            <p className="lead">
+                <i class="fas fa-user" /> Sign into your account
+            </p>
+            <form className="form" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email address"
+                        name="email"
+                        value={email}
+                        onChange={onChange}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="placeholder"
+                        name="password"
+                        onChange={onChange}
+                        minLength = "6"
+                    />
+                </div>
+                <input type="submit" className="btn btn-pimrary" value="Login" />
+            </form>
+
+            <p className="my-1">
+                Dont have an account? <Link to="/register">Sign up</Link>
+            </p>
+        </section>
+    )
+}};
+
+Login.propTypes = {
+    login: propTypes.func.isRequired,
+    isAuthenticated: PropType.bool
+}
+
