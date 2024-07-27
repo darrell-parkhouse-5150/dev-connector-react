9deddaba0076de18c0118335012dd15830c1b323
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-07-27 13:34:54 -0500

    defined the auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -18,3 +18,50 @@ router.get('/', auth, async (req, res) => {
         res.status(503).json({ msg: "internal server error"})
     }
 })
+
+router.post('/',
+    check('email', 'please include a valid email').isEmail(),
+    check('password', 'password is required').exists(),
+    async (req, res) => {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        const { email, password } = req.body
+
+        try {
+            let user = await User.findOne({ email })
+
+            if (!user) {
+                return res.status(400).json({ errors: [{ msg: 'invalid credentials'}]})
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({ errors: [{ msg: 'invalid credentials'}]})
+            }
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(
+                payload, config.get('jwtSecret'), { expiresIn: '5 days' }, (err, token) => {
+                    if (err) throw err;
+
+                    res.json({token});
+                }
+            )
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send('server error')
+        }
+    }
+)
+
+module.exports = router;
