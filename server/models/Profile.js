191bdf5134c61a023fe3e9c9b0e0669ce42b66fa
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-07-27 12:34:09 -0500

    defined the profile model

---
+++ server/models/Profile.js
@@ -0,0 +1,123 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: Schema.Tyes.ObjectId,
+        ref: 'user'
+    },
+    company: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true
+    },
+
+    skills: {
+        type: [String],
+        required: true
+    },
+    bio: {
+        type: String
+    },
+    githubUsername: {
+        type: String
+    },
+
+    experience: [
+        {
+            title: {
+                type: String,
+                required: true
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date,
+                required: true
+            },
+            current: {
+                type: Boolean,
+                default: false,
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+
+            fos: {
+                type: String,
+                required: true
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date,
+                required: true
+            },
+
+            current: {
+                type: Boolean,
+                default: false
+            },
+
+            description: {
+                type: String
+            }
+        }
+    ],
+
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('profile', ProfileSchema)
