14f36b9082f5bd2c38aeead302c6afa0b81f3dc5
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-07-27 11:20:25 -0500

    defined get get profile function

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -28,3 +28,18 @@ export const getCurrentProfile = () => async (dispatch) => {
         })
     }
 }
+export const getProfiles = () => async (dispatch) => {
+    try {
+        const res = await api.get('/profile/')
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        })
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status }
+        })
+    }
+}
