711a49b7a5652a2c2c1bcef9bfb4342726417f6c
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-07-27 11:11:32 -0500

    defined the getCurrentProfile

---
+++ client/src/actions/profile.js
@@ -0,0 +1,30 @@
+import api from '../util/api'
+import setAlert from './alert'
+
+
+import {
+    GET_PROFILE,
+    GET_PROFILES,
+    PROFILE_ERROR,
+    UPDATE_PROFILE,
+    CLEAR_PROFILE,
+    ACCOUNT_DELETED,
+    GET_REPOS,
+    NO_REPOS
+} from './types'
+
+export const getCurrentProfile = () => async (dispatch) => {
+    try {
+        const res = await api.get('/profile/me')
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status }
+        })
+    }
+}
