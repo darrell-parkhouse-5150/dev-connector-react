df5ae10069857b923f668a2a7fb7edc95f6734f8
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-08-04 12:01:40 -0500

    defined the useEffect

---
+++ client/src/components/profile-forms/ProfileForm.js
@@ -0,0 +1,54 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import { Link, useMatch, useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createProfile, getCurrentProfile } from '../../actions/profile';
+
+const initialState = {
+    company: '',
+    website: '',
+    location: '',
+    status: '',
+    skills: '',
+    gethubusername: '',
+    bio: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    youtube: '',
+    instagram: ''
+};
+
+const ProfileForm = ({ 
+    profile: { profile, loading},
+    createProfile,
+    getCurrentProfile
+}) => {
+    const [formData, setFormData] = useState(initialState)
+    const createProfile = useMatch('/create-profile')
+    const [displaySocalInputs, toggleSocialInputs] = useState(false)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!profile) getCurrentProfile()
+
+        if (!loading && profile) {
+            profileData = { ...initialState }
+
+            for (const key in profile) {
+                if (key in profile)
+                    profileData[key] = profile[key]
+            }
+
+            for (const key in profile.social) {
+                if (key in profileData)
+                    profileData[key] = profile.social[key]
+            }
+
+            if (Array.isArray(profileData.skills))
+                profileData.skills = profiledata.skills.join(', ')
+
+            setFormData(profileData)
+        }
+    }, [loading, getCurrentProfile, profile])
+}
