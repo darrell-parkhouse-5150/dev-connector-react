0642c69ad5eae71c18d37a0935603719167c332d
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-08-04 12:06:31 -0500

    defined the on change for the selection element

--- client/src/components/profile-forms/ProfileForm.js
+++ client/src/components/profile-forms/ProfileForm.js
@@ -1,54 +1,66 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { Link, useMatch, useNavigate } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { createProfile, getCurrentProfile } from '../../actions/profile';
+import React, { Fragment, useState, useEffect } from "react";
+import { Link, useMatch, useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProfile, getCurrentProfile } from "../../actions/profile";
 
 const initialState = {
-    company: '',
-    website: '',
-    location: '',
-    status: '',
-    skills: '',
-    gethubusername: '',
-    bio: '',
-    twitter: '',
-    facebook: '',
-    linkedin: '',
-    youtube: '',
-    instagram: ''
+    company: "",
+    website: "",
+    location: "",
+    status: "",
+    skills: "",
+    gethubusername: "",
+    bio: "",
+    twitter: "",
+    facebook: "",
+    linkedin: "",
+    youtube: "",
+    instagram: "",
 };
 
-const ProfileForm = ({ 
-    profile: { profile, loading},
-    createProfile,
-    getCurrentProfile
-}) => {
-    const [formData, setFormData] = useState(initialState)
-    const createProfile = useMatch('/create-profile')
-    const [displaySocalInputs, toggleSocialInputs] = useState(false)
-    const navigate = useNavigate()
+const ProfileForm = ({ profile: { profile, loading }, createProfile, getCurrentProfile }) => {
+    const [formData, setFormData] = useState(initialState);
+    const createProfile = useMatch("/create-profile");
+    const [displaySocalInputs, toggleSocialInputs] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        if (!profile) getCurrentProfile()
+        if (!profile) getCurrentProfile();
 
         if (!loading && profile) {
-            profileData = { ...initialState }
+            profileData = { ...initialState };
 
             for (const key in profile) {
-                if (key in profile)
-                    profileData[key] = profile[key]
+                if (key in profile) profileData[key] = profile[key];
             }
 
             for (const key in profile.social) {
-                if (key in profileData)
-                    profileData[key] = profile.social[key]
+                if (key in profileData) profileData[key] = profile.social[key];
             }
 
-            if (Array.isArray(profileData.skills))
-                profileData.skills = profiledata.skills.join(', ')
+            if (Array.isArray(profileData.skills)) profileData.skills = profiledata.skills.join(", ");
 
-            setFormData(profileData)
+            setFormData(profileData);
         }
-    }, [loading, getCurrentProfile, profile])
-}
+    }, [loading, getCurrentProfile, profile]);
+
+    const {
+        company,
+        website,
+        location,
+        status,
+        skills,
+        gethubusername,
+        bio,
+        twitter,
+        facebook,
+        linkedin,
+        youtube,
+        instagram,
+    } = formData;
+
+    const onChange = (e) => {
+        setFormData({ ...FormData, [e.target.name]: e.target.value})
+    }
+};
