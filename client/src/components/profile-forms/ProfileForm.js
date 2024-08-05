17d1eba23d7d4db06f9f697a60c16405fbdbf90a
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-08-04 23:40:07 -0500

    defined the job title select component and added the inputs for company and website"

--- client/src/components/profile-forms/ProfileForm.js
+++ client/src/components/profile-forms/ProfileForm.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { Link, useMatch, useNavigate } from "react-router-dom";
-import PropTypes from "prop-types";
+import propType from "prop-types";
 import { connect } from "react-redux";
 import { createProfile, getCurrentProfile } from "../../actions/profile";
 
@@ -61,6 +61,72 @@ const ProfileForm = ({ profile: { profile, loading }, createProfile, getCurrentP
     } = formData;
 
     const onChange = (e) => {
-        setFormData({ ...FormData, [e.target.name]: e.target.value})
+        setFormData({ ...FormData, [e.target.name]: e.target.value});
     }
+
+    const onSubmit = (e) => {
+        const editing = profile ? true : false;
+        e.preventDefault();
+
+        createProfile(formData, editing).then(() => {
+            if (!editing) navigate('/dashboard')
+        });
+    };
+
+    return (
+        <section className="container">
+            <h1 className="laerge text-primary">
+                { createProfile ? 'Create your profile' : 'Edit your profile'}
+            </h1>
+            <p className="load">
+                <i className="fas fa-use" />
+                {
+                    createProfile 
+                        ? "Let's get some information to make your profile"
+                        : "Add some changed to your profile"
+                }
+            </p>
+            <small>* = required field</small>
+            <form className="form" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <select name="status" value={status} onChange={onChange}>
+                        <option>* Select professional status</option>
+                        <option value="dev">developer</option>
+                        <option value="jr dev">Jr Developer</option>
+                        <option value="sr dev">Sr Developer</option>
+                        <option value="manager">Manager</option>
+                        <option value="student">Student</option>
+                        <option value="instructor">Instructor</option>
+                        <option value="intern">Intern</option>
+                        <option value="other">other</option>
+                    </select>
+                    <small className="form-text">Give us an idea of where you are at in your career</small>
+                </div>
+                <div className="form-group">
+                    <label for="company"></label>
+                    <input type="text" placeholder="" name="company" value={company} onChange={onChange} id="company"/>
+        
+                    <small className="form-text">could be your own company or one you work for</small>
+                </div>
+                <div className="form-group">
+                    <label for="text"></label>
+                    <input type="text" placeholder="website" name="website" value={website} onChange={onChange} id="text"/>
+                    <small className="form-text"></small>
+                </div>
+        
+            </form>
+        </section>
+    )
 };
+
+ProfileForm.propTypes={
+    createProfile: propType.func.isRequired,
+    getCurrentProfile: propType.func.isRequired,
+    profile: propType.object.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+    profile: state.profile
+});
+
+export default connect(mapStateToProps, { createProfile, getCurrentProfile })(ProfileForm);
