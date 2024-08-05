78190d208b9cc57e47683be03bcf419dc93cab37
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-08-04 23:35:06 -0500

    added routes for login, register, dashboard, alert, and post. the post is a private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import  { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import ProfileForm from './components/profile-forms/ProfileForm';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
+import NotFound from './components/layout/NotFound';
+import PrivateRoute from './components/routing/PrivateRoute';
+import { LOGOUT } from './actions/types';
+/**
+ * Renders the main application component with routing functionality.
+ * Utilizes React Router to define routes for Login, Register, Dashboard, Alert, and individual Posts.
+ * PrivateRoute component is used to protect the route for displaying a specific Post.
+ */
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="login" component={Login} />
+                <Route path="register" component={Register} />
+                <Route path="dashboard" component={Dashboard} />
+                <Route path="alert" component={Alert} />
+                <Route path="posts/:id" element={<PrivateRoute></PrivateRoute>} component={Post}></Route>
+            </Routes>
+        </Router>
+    )
 }
-
-export default App
