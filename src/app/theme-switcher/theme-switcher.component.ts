f36e13b9a4efaa3e361d1f7102ed4c69537af410
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-08-04 23:56:53 -0500

    renamed the selecter to ng-theme-switcher

---
+++ src/app/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-theme-switcher',
+  standalone: true,
+  imports: [],
+  templateUrl: './theme-switcher.component.html',
+  styleUrl: './theme-switcher.component.scss'
+})
+export class ThemeSwitcherComponent {
+    onSwitchTheme() {
+
+    }
+}
