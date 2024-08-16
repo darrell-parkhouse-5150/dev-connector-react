b2f4273b24302781ff483eb441fff8a962301251
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-08-16 11:14:38 -0500

    defined the No found component when a user searchs and nothing is found

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <section className="container">
+            <h1 className="x-large text-primary">
+                <i className="fas fa-exclamation-triangle" /> page not found
+            </h1>
+            <p className="large">sorry, this page does not exist</p>
+        </section>
+    )
+}
+
+export default NotFound
