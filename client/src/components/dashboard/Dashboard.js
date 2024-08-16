1d3a1d500afc0329943c8a3fd6e5d2c450f468ea
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-08-16 08:08:13 -0500

    defined the propTypes and mapStateToProps and simply exported the class

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,11 +3,9 @@ import { Link } from 'react-router-dom'
 import propTypes from 'prop-types'
 import { connect } from 'react=redux'
 import DashboardActions from './DashboardActions'
-
 import Experience from './Experience'
 import Education from './Education'
 import { getCurrentProfile, deleteAccount } from '../../actions/profile'
-
 
 const Dashboard = ({
     getCurrentProfile,
